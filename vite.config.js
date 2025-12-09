fc7c618785861eba49cc3e44f77134830fbf46c4
Sofi <sofiarchlinux@gmail.com>
2025-12-09 04:30:21 +0300

    CONFIG FOR ROOT DOMAIN

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // <--- Change this to just a single slash
+  base: "/",  // <--- JUST A SLASH (This is the key difference)
 })
